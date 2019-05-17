59500a9a0584328d52958f2164a4b50880af11c4
TEMPDAROS <daniel.ros@hm.com>
2019-05-17 12:34:52 +0200

    sort of working agent

--- agent.py
+++ agent.py
@@ -8,6 +8,8 @@ import hashlib
 class Player:
     def __init__(self, indicator):
         self.indicator = indicator
+        self.wins = 0
+        self.draws = 0
 
     # Select a random place for the player
 
@@ -27,35 +29,44 @@ class Player:
         
     def bad_action(self):
         return 0
+    
+    def end_game_evaluation(self, reward):
+        return 0
 
 
 class Human(Player):
     def __init__(self, indicator):
         self.indicator = indicator
         self.action = ()
+        self.wins = 0
+        self.draws = 0
 
     def choose_action(self, board):
-        selection = self.possibilities(board)
-        while self.action not in selection:
-            action_raw = input("make a choice, type x,y: ").split(',')
-            self.action = tuple(int(cordinate) for cordinate in action_raw)
-            print(self.action in selection)
+        #selection = self.possibilities(board)
+        #while self.action not in selection:
+        action_raw = input("make a choice, type x,y: ").split(',')
+        if action_raw == "q":
+            return "quit"
+        self.action = tuple(int(cordinate) for cordinate in action_raw)
         return self.action
 
 
 class Agent(Player):
-    def __init__(self, indicator, gamma, debug=False):
+    def __init__(self, indicator, gamma, epsilon, debug=False):
         self.gamma = gamma
         self.indicator = indicator
         self.statesFound = 0
         self.debug = debug
         self.qTable = np.zeros(shape=[19668, 9])
         self.stateIndexMap = {}
-        self.epsilon = 0.1
+        self.epsilon = epsilon
         self.moves = []
-        self.max_epsilon = 1.0
+        self.max_epsilon = 0.85
         self.min_epsilon = 0.01
         self.decay_rate = 0.01
+        self.discount_factor = 0.01
+        self.wins = 0
+        self.draws = 0
         self.allActions = [(0, 0), (0, 1), (0, 2),
                            (1, 0), (1, 1), (1, 2),
                            (2, 0), (2, 1), (2, 2)]
@@ -65,17 +76,25 @@ class Agent(Player):
         chosenAction = ()
         actionIndex = 99
         stateIndex = self.stateIndexMap[self._hashBoardState(board)]
-        if random.uniform(0, 1) < self.epsilon:
+        if  random.random() < self.epsilon:
+            # action from Q-table
+            if self.debug:
+                print('exploit')
+                print('available Q-values for state: ', self.qTable[stateIndex])
+                print('actionIndex: ', np.argmax(self.qTable[stateIndex]))
+            actionIndex = np.argmax(self.qTable[stateIndex])
+            chosenAction = self.allActions[actionIndex]
+        else:
+            if self.debug:
+                print('explore, epsilon: ', self.epsilon)
             # random from available actions
             chosenAction = random.choice(self.possibilities(board))
             actionIndex = self.allActions.index(chosenAction)
-        else:
-            # action from Q-table
-            actionIndex = np.argmax(self.qTable[stateIndex])
-            chosenAction = self.allActions[actionIndex]
-
-        self.epsilon = self.min_epsilon + \
-            (self.max_epsilon-self.min_epsilon) * np.exp(-0.1*self.epsilon)
+            
+        #self.epsilon = self.min_epsilon + \
+        #    (self.max_epsilon-self.min_epsilon) * np.exp(-0.1*self.epsilon)
+        if self.epsilon < self.max_epsilon:
+            self.epsilon += self.decay_rate            
         
         self.moves.append((stateIndex, actionIndex))
         if self.debug:
@@ -84,12 +103,32 @@ class Agent(Player):
     
     def bad_action(self):
         lastMove = self.moves.pop()
-        self.qTable[lastMove[0]][lastMove[1]] = -1
+        self.qTable[lastMove[0]][lastMove[1]] = self.q_value_for(lastMove) - 1000
+        if self.debug:
+            print('agent bad move: ', lastMove)
+            print('updated q values for state: ', self.qTable[lastMove[0]])
+    
+    def end_game_evaluation(self, reward):
+        if self.debug:
+            print('------- end game eval -------------- \n', len(self.moves), self.moves)
+        
+        while len(self.moves) > 0:
+            previousMove = self.moves.pop()
+            self.qTable[previousMove[0]][previousMove[1]] = self.q_value_for(previousMove) + (reward * self.discount_factor)
+            reward = self.discount_factor * reward
+
+    def set_epsilon(self, epsilon):
+        self.epsilon = epsilon
+
+    def q_value_for(self, move):
+        return self.qTable[move[0]][move[1]]
 
     def _normalizeIndicators(self, x):
         return x if (x == 0 or x == self.indicator) else 9
 
     def _setNewState(self, stateHash):
+        #if self.debug:
+        #print('new state found: ', stateHash)
         self.stateIndexMap[stateHash] = self.statesFound
         self.statesFound +=1
 
@@ -101,7 +140,6 @@ class Agent(Player):
             self._setNewState(stateHash)
         
         return stateHash
-
             
 
 
--- game.py
+++ game.py
@@ -66,6 +66,16 @@ def diag_win(board, playerIndicator):
     for x in range(len(board)):
         if board[x, x] != playerIndicator:
             win = False
+    
+    if win == True:
+        # left to right
+        return (win)
+    
+    win = True
+    for x in range(len(board)):
+        if board[x, (2 - x)] != playerIndicator:
+            win = False
+    
     return (win)
 
 
@@ -87,29 +97,70 @@ def evaluate(board, playerlist):
 
 
 # Main function to start the game
-def play_game(player1, player2):
+def play_game(player1, player2, training):
     board, winner, counter = create_board(), 0, 1
     while winner == 0:
-        print(board)
         for player in [player1, player2]:
+            if not training:
+                print('current board state: \n', board)
             action = player.choose_action(board)
             # handle choosen action
-            print('counter: ', counter)
+            #print('counter: ', counter)
             while action not in possibilities(board):
+                if action == "quit":
+                    return -1
                 player.bad_action()
-                print(player.choose_action(board))
                 action = player.choose_action(board)
 
             board[action] = player.indicator
             counter += 1
             winner = evaluate(board, [player1, player2])
             if winner != 0:
-                print(board)
+                if not training:
+                    print('winning boardState: \n', board)
+                if winner == player1.indicator:
+                    player1.end_game_evaluation(100)
+                    player1.wins += 1
+                    player2.end_game_evaluation(-100)
+                elif winner == player2.indicator:
+                    player1.end_game_evaluation(-100)
+                    player2.end_game_evaluation(100)
+                    player2.wins += 1
+                else:
+                    player1.end_game_evaluation(0)
+                    player2.end_game_evaluation(0)
+                    player1.draws += 1
+                    player2.draws += 1
                 break
     return (winner)
 
-
 # Driver Code
 human = Human(8)
-agent = Agent(0, 3, True)
-print("Winner is: " + str(play_game(human, agent)))
+agent = Agent(1, 0.9, 0.1, False)
+agent2 = Agent(2, 0.9, 0.1, False)
+agent3 = Agent(3, 0.9, 0.1, False)
+for i in range(50000):
+    play_game(agent, agent2, True)
+    play_game(agent2, agent, True)
+    if i % 1000 == 0:
+        print(i)
+        print('agent1 wins: ' + str(agent.wins), 'agent2 wins: ' + str(agent2.wins))
+        print('draws ' + str(agent.draws))
+agent.set_epsilon(1)
+agent2.set_epsilon(1)
+agent.wins = 0
+agent.draws = 0
+for i in range(350):
+    play_game(agent, agent3, True)
+    play_game(agent3, agent, True)
+    if i % 10 == 0:
+        print(i)
+        print(' agent1 wins: ' + str(agent.wins),
+              ' agent3 wins: ' + str(agent3.wins))
+        print('draws ' + str(agent.draws))
+agent.debug = True
+agent2.debug = True
+agent3.debug = True
+for i in range(700):
+    print("Winner is: " + str(play_game(agent3, human, False)))
+    print("Winner is: " + str(play_game(human, agent3, False)))
